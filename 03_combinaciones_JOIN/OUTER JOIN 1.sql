7003a918c8017cbe18f5be78fcdc35b69061789d
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-23 20:33:33 -0300

    Update OUTER JOIN 1.sql

--- 03_combinaciones_JOIN/OUTER JOIN 1.sql
+++ 03_combinaciones_JOIN/OUTER JOIN 1.sql
@@ -1,3 +1,10 @@
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: LEFT JOIN
+-- Descripción: Comprender uso de LEFT JOIN
+-- Objetivo: Entregar una lista de todas las compañias clientes y de sus contactos (Nombre y Apellido),
+-- mostrando el número de pedido de compra y el total del pedido para cada pedido. Clientes sin pedidos
+-- deberán figurar al final de la tabla con los campos número de pedido de compra y total del pedido como NULL.
+
 SELECT  c.CompanyName,
 		c.FirstName,
 		c.LastName,
@@ -8,3 +15,4 @@ FROM SalesLT.Customer AS c
 LEFT JOIN SalesLT.SalesOrderHeader as oh
 	ON c.CustomerID = oh.CustomerID
 ORDER BY oh.PurchaseOrderNumber DESC
+
