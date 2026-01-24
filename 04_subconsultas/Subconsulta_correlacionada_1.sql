5b2cb2a640eca60e6147ffb725f21f83aa1580f2
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-24 17:56:32 -0300

    Add files via upload

---
+++ 04_subconsultas/Subconsulta_correlacionada_1.sql
@@ -0,0 +1,6 @@
+SELECT p.ProductID, p.Name, p.StandardCost, p.ListPrice ,
+	(SELECT  AVG(o.UnitPrice)
+	FROM SalesLT.SalesOrderDetail AS o
+	WHERE p.productID= o.ProductID) AS AvgSellingPrice
+FROM SalesLT.Product AS p
+Order by P.ProductID
