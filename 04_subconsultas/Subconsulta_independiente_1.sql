f16882c876c2a41dfb2f9fb45185ae446f3aaa19
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-24 17:13:08 -0300

    Update Subconsulta_independiente_1.sql

--- 04_subconsultas/Subconsulta_independiente_1.sql
+++ 04_subconsultas/Subconsulta_independiente_1.sql
@@ -1,6 +1,15 @@
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: Subqueries
+-- Descripción: Comprender uso de Subqueries
+-- Objetivo : Obtener los productos cuyo precio de venta recomendado es mayor
+-- que el precio medio de venta real.
+
 SELECT p.ProductID, p.Name, p.ListPrice
 FROM SalesLT.Product AS p
 WHERE   p.ListPrice >
 	(SELECT AVG(UnitPrice)
 	FROM SalesLT.SalesOrderDetail AS od) 
 ORDER BY p.ProductID
+
+-- Se utilizan Subconsultas independientes en este ejercicio. La subconsulta interior no hace referencia
+-- a la consulta exterior
