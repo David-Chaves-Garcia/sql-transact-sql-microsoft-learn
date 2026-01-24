28fcf743f75f771b1a967292f4f874650500d84d
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-24 10:42:59 -0300

    Update OUTER JOIN_2.sql

--- 03_combinaciones_JOIN/OUTER JOIN_2.sql
+++ 03_combinaciones_JOIN/OUTER JOIN_2.sql
@@ -1,3 +1,9 @@
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: LEFT JOIN
+-- Descripción: Comprender uso de LEFT JOIN
+-- Objetivo : Entregar una lista de los clientes sin dirección donde figure ID del cliente, 
+-- nombre de la compañía, contacto (nombre y apellidos) y número de teléfono.
+
 SELECT c.CustomerID, c.CompanyName, c.FirstName+' '+c.LastName AS ContactName
 FROM SalesLT.Customer AS c
 LEFT JOIN SalesLT.CustomerAddress AS ca
