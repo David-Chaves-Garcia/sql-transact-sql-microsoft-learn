7e43309e1890159c182aa27cad795429205eeffb
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-24 18:01:40 -0300

    Update Subconsulta_correlacionada_1.sql

--- 04_subconsultas/Subconsulta_correlacionada_1.sql
+++ 04_subconsultas/Subconsulta_correlacionada_1.sql
@@ -1,6 +1,14 @@
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: Subqueries
+-- Descripción: Comprender uso de Subqueries
+-- Objetivo : Obtener coste, precio de venta recomendado y media del precio de venta
+-- para cada producto
+
 SELECT p.ProductID, p.Name, p.StandardCost, p.ListPrice ,
-	(SELECT  AVG(o.UnitPrice)
+	(SELECT  AVG(o.UnitPrice) -- la subconsulta en este caso va en el statement SELECT
 	FROM SalesLT.SalesOrderDetail AS o
 	WHERE p.productID= o.ProductID) AS AvgSellingPrice
 FROM SalesLT.Product AS p
 Order by P.ProductID
+
+-- La subconsulta es correlacionada ya que en el WHERE hace referencia a la consulta externa
