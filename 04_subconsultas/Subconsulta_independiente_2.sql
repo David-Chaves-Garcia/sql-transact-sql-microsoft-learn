4f2ac7b2f1ecdc78938db35e98af1a954c9ee196
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-24 17:35:55 -0300

    Update Subconsulta_independiente_2.sql

--- 04_subconsultas/Subconsulta_independiente_2.sql
+++ 04_subconsultas/Subconsulta_independiente_2.sql
@@ -1,3 +1,9 @@
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: Subqueries
+-- Descripción: Comprender uso de Subqueries
+-- Objetivo : Obtener los productos cuyo precio de venta recomendado sea 100 o más
+-- y haya sido vendido por menos de 100
+
 SELECT p.ProductID, p.Name, p.ListPrice
 FROM SalesLT.Product AS p
 WHERE p.ProductID IN
@@ -6,3 +12,5 @@ WHERE p.ProductID IN
 	WHERE od.UnitPrice < 100.00)
 AND p.ListPrice >= 100.00
 ORDER BY p.ProductID
+
+-- Es una subquery independiente
