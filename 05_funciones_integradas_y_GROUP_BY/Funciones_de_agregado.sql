59da29b116030e8f966cd7d7c25f8185a3791563
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-26 15:40:10 -0300

    Update Funciones_de_agregado.sql

--- 05_funciones_integradas_y_GROUP_BY/Funciones_de_agregado.sql
+++ 05_funciones_integradas_y_GROUP_BY/Funciones_de_agregado.sql
@@ -1,8 +1,14 @@
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: Funciones Integradas
+-- Descripción: Comprender uso de Funciones Integradas (de agregado)
+-- Objetivo : Obtener el número total de ventas para cada producto donde ListPrice sea > 1000
+-- y donde el número total de ventas del producto sea > 20
+
 SELECT p.Name, SUM(od.OrderQty) AS TotalSales
 FROM SalesLT.Product AS p
 JOIN SalesLT.SalesOrderDetail AS od
 	ON p.ProductID= od.ProductID
-WHERE p.ListPrice>1000
+WHERE p.ListPrice>1000 -- Filtra antes de hacer agrupaciones
 GROUP BY p.Name
-HAVING SUM(od.OrderQty)>20
+HAVING SUM(od.OrderQty)>20 -- Filtra los grupos una vez constituídos (siempre después de GROUP BY)
 ORDER BY TotalSales DESC
