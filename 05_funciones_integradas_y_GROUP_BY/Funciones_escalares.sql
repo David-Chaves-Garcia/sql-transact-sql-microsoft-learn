15ac79e117488953278308c7a3f0a1dd1b50d7e2
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-26 15:13:47 -0300

    Update Funciones_escalares.sql

--- 05_funciones_integradas_y_GROUP_BY/Funciones_escalares.sql
+++ 05_funciones_integradas_y_GROUP_BY/Funciones_escalares.sql
@@ -1,6 +1,6 @@
 -- Curso: Microsoft Learn - Transact-SQL
 -- Tema: Funciones Integradas
--- Descripción: Comprender uso de Funciones Integradas
+-- Descripción: Comprender uso de Funciones Integradas (escalares)
 -- Objetivo : Obtener para cada pedido de venta: Order ID, Freight Cost redondeado a 2 decimales,
 -- ShipMethod denominado como ShippingMethod y en minúsculas, año de ShipDate denominado como ShipYear,
 -- mes de ShipDate denominado como ShipMonth y en formato nombre del mes, día de ShipDate denominado como ShipDay
@@ -13,3 +13,4 @@ SELECT SalesOrderID,
 		DAY(ShipDate) AS ShipDay		
 
 FROM SalesLT.SalesOrderHeader
+
