2e0b1182a411512240c1acfee49256f30ea96068
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-28 12:13:24 -0300

    Update Delete_data.sql

--- 06_modificación_de_datos/Delete_data.sql
+++ 06_modificación_de_datos/Delete_data.sql
@@ -1,2 +1,12 @@
- DELETE FROM SalesLT.CallLog
- WHERE CallTime < DATEADD(dd, -7, GETDATE());
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: Modificación de datos
+-- Descripción: Consultas de eliminación de datos
+
+
+-- ELIMINAMOS FILAS DE LA TABLA QUE CUMPLAN LA CLÁUSULA WHERE
+DELETE FROM SalesLT.CallLog
+ WHERE CallTime < DATEADD(dd, -7, GETDATE()); -- Elimina las filas en las que la fecha sea anterior a hace 7 días
+
+
+-- ELIMINAMOS TODAS LAS FILAS DE LA TABLA
+ TRUNCATE TABLE SalesLT.CallLog;
