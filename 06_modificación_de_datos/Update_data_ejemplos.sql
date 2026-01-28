1588dd7a0290d724662a609fbadd9df3e01b184d
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-28 12:01:52 -0300

    Update Update_data_ejemplos.sql

--- 06_modificación_de_datos/Update_data_ejemplos.sql
+++ 06_modificación_de_datos/Update_data_ejemplos.sql
@@ -1,3 +1,20 @@
- UPDATE SalesLT.CallLog
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: Modificación de datos
+-- Descripción: Consultas de actualización de datos
+
+-- ACTUALIZAMOS TODOS LOS VALORES DE UNA COLUMNA DONDE SE CUMPLA LA CLÁUSULA WHERE
+UPDATE SalesLT.CallLog
  SET Notes = 'No notes'
  WHERE Notes IS NULL;
+
+
+-- ACTUALIZAMOS MÚLTIPLES COLUMNAS A LA VEZ
+UPDATE SalesLT.CallLog
+ SET SalesPerson = '', PhoneNumber = ''
+
+
+ -- ACTUALIZACIÓN BASADA EN UNA CONSULTA SELECT
+ UPDATE SalesLT.CallLog
+ SET SalesPerson = c.SalesPerson, PhoneNumber = c.Phone
+ FROM SalesLT.Customer AS c
+ WHERE c.CustomerID = SalesLT.CallLog.CustomerID;
