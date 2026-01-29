d0574cc2157d853ba7a55b126aba4e14bc20b3bc
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-29 11:53:22 -0300

    Update Update_data_challenge.sql

--- 06_modificación_de_datos/Update_data_challenge.sql
+++ 06_modificación_de_datos/Update_data_challenge.sql
@@ -1,6 +1,22 @@
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: Modificación de datos
+
+-- Objetivo Consulta 1 :Actualizar el precio de los productos cuya categoría
+-- sea 'Bells and Horns', aumentándolo un 10% del precio que tienen actualmente.
+
 UPDATE SalesLT.Product
 SET ListPrice= ListPrice*1.10
 WHERE ProductCategoryID=
-	(SELECT ProductCategoryID
+	(SELECT ProductCategoryID	
 	FROM SalesLT.ProductCategory
 	WHERE Name= 'Bells and Horns')
+
+
+-- Objetivo Consulta 2: Actualizar los productos con ProductCatgegoryID = 37 como productos 
+-- que tienen DiscontinuedDate, es decir, como productos que se han dejado de vender. Todos
+-- los de esa categoría menos uno en concreto que se sigue vendidendo.
+
+UPDATE SalesLT.Product
+SET DiscontinuedDate = GETDATE()
+WHERE ProductCategoryID= 37 
+AND ProductNumber <>'LT-L123'
