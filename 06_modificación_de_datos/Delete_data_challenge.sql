2782abfcb211d9279062e74f64c57f9ea01c7a85
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-29 12:06:52 -0300

    Update Delete_data_challenge.sql

--- 06_modificación_de_datos/Delete_data_challenge.sql
+++ 06_modificación_de_datos/Delete_data_challenge.sql
@@ -1,3 +1,7 @@
+-- Curso: Microsoft Learn - Transact-SQL
+-- Tema: Modificación de datos
+-- Objetivo Borrar los datos de productos cuya categoría sea 'Bells and Horns'
+
 DELETE FROM SalesLT.Product
 WHERE ProductCategoryID=
 	(SELECT ProductCategoryID
@@ -9,3 +13,7 @@ WHERE ProductCategoryID=
 	(SELECT ProductCategoryID
 	FROM Saleslt.ProductCategory
 	WHERE Name= 'Bells and Horns');
+
+-- Si queremos eliminar los datos de una tabla padre (con claves foráneas) primero debemos
+-- eliminar los datos de la tabla hija, sino dejaríamos a la tabla hija apuntando a algo que 
+-- ya no existe, y por lo tanto se rompería la integridad de los datos
