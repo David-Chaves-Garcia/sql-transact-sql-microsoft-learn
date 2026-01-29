9e8b91ecadf9a72f2c2e5e91d9a7f7f9122ee3a5
David-Chaves-Garcia <156066336+David-Chaves-Garcia@users.noreply.github.com>
2026-01-29 10:51:19 -0300

    Add files via upload

---
+++ 06_modificación_de_datos/Insert_data_challenge.sql
@@ -0,0 +1,7 @@
+INSERT INTO SalesLT.Product (Name, ProductNumber, StandardCost, ListPrice, ProductCategoryID, SellStartDate)
+VALUES ('LED Lights', 'LT-L123', 2.56, 12.99, 37, GETDATE());
+
+SELECT SCOPE_IDENTITY();
+
+SELECT * FROM SalesLT.Product
+WHERE ProductID= SCOPE_IDENTITY();
